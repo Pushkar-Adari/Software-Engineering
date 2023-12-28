a272a54dab74383fb3ee6141888e37664acc6d76
Pushkar-Adari <101546155+Pushkar-Adari@users.noreply.github.com>
2023-12-28 15:08:44 +0530

    new

--- multiplesource.py
+++ multiplesource.py
@@ -1,9 +1,17 @@
-def solve_eqn(a,b,c,x):
-    return a*x**2 + b*x + c 
-num_sets = int(input("Coefficient: "))
-x = float(input("x: "))
-for i in range(num_sets):
-    a = float(input("a: "))
-    b = float(input("b: "))
-    c = float(input("c: "))
-    result = solve_eqn(a,b,c,x)
+import math
+
+def quadratic_solution(a, b, c):
+    delta = b**2 - 4*a*c  # Use '*' for multiplication, not '%'
+    if delta >= 0:
+        root1 = (-b + math.sqrt(delta)) / (2*a)
+        root2 = (-b - math.sqrt(delta)) / (2*a)
+        return root1, root2
+    else:
+        return "Complex roots - No real solution"
+
+coefficient_sets = [(2, -6, 1), (1, 4, -6), (-1, 6, -4)]
+
+for coefficients in coefficient_sets:
+    a_set, b_set, c_set = coefficients
+    roots_set = quadratic_solution(a_set, b_set, c_set)
+    print(f"Set input roots: {roots_set}")
